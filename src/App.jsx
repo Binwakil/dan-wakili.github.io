2082977d589345f63c1466711a467500712a95f6
ALMUSTAPHA WAKILI <almustaphawakili@gmail.com>
2023-04-12 10:21:05 +0100

    sataged

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,8 @@ const App = () => {
       <Hireme />
       <Contact />
       <footer className="p-3 text-center">
-        <h6 className="mb-3">JOHN ALEX</h6>
-        <p>codeaprogram © All CopyRights Reserved 2022</p>
+        <h6 className="mb-3">Almustapha Wakili</h6>
+        <p>Binwakili © All CopyRights Reserved 2022</p>
       </footer>
     </div>
   );
